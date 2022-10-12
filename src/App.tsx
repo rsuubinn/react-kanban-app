9796eabcb7165cedd4fde16633d60dfa17ef8969
rsuubinn <rsuubinn@gmail.com>
2022-10-12 22:23:57 +0900

    #6.4 Styles and Placeholders

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,127 @@
 import React from "react";
+import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { darkTheme } from "./theme";
+
+const GlobalStyle = createGlobalStyle`
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, menu, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup,
+main, menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure,
+footer, header, hgroup, main, menu, nav, section {
+  display: block;
+}
+/* HTML5 hidden-attribute fix for newer browsers */
+*[hidden] {
+    display: none;
+}
+body {
+  line-height: 1.1;
+  background-color: ${(props) => props.theme.bgColor};
+}
+menu, ol, ul {
+  list-style: none;
+}
+blockquote, q {
+  quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+  content: '';
+  content: none;
+}
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+ *{
+  box-sizing: border-box;
+ }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  max-width: 480px;
+  width: 100%;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Boards = styled.div`
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
+  width: 100%;
+`;
+
+const Board = styled.div`
+  background-color: ${(props) => props.theme.boardColor};
+  padding: 20px 10px;
+  padding-top: 30px;
+  border-radius: 5px;
+  min-height: 200px;
+`;
+
+const Card = styled.div`
+  background-color: ${(props) => props.theme.cardColor};
+  border-radius: 5px;
+  margin-bottom: 5px;
+  padding: 10px;
+`;
+
+const toDos = ["a", "b", "c", "d", "e", "f"];
 
 function App() {
   const onDragEnd = () => {};
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Droppable droppableId="one">
-        {(provided) => (
-          <ul ref={provided.innerRef} {...provided.droppableProps}>
-            <Draggable draggableId="first" index={0}>
+    <ThemeProvider theme={darkTheme}>
+      <GlobalStyle />
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Wrapper>
+          <Boards>
+            <Droppable droppableId="one">
               {(provided) => (
-                <li ref={provided.innerRef} {...provided.draggableProps}>
-                  <span {...provided.dragHandleProps}>💖</span>
-                  one
-                </li>
+                <Board ref={provided.innerRef} {...provided.droppableProps}>
+                  {toDos.map((toDo, index) => (
+                    <Draggable draggableId={toDo} index={index}>
+                      {(provided) => (
+                        <Card
+                          ref={provided.innerRef}
+                          {...provided.dragHandleProps}
+                          {...provided.draggableProps}
+                        >
+                          {toDo}
+                        </Card>
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </Board>
               )}
-            </Draggable>
-            <Draggable draggableId="second" index={1}>
-              {(provided) => (
-                <li ref={provided.innerRef} {...provided.draggableProps}>
-                  <span {...provided.dragHandleProps}>💖</span>
-                  two
-                </li>
-              )}
-            </Draggable>
-          </ul>
-        )}
-      </Droppable>
-    </DragDropContext>
+            </Droppable>
+          </Boards>
+        </Wrapper>
+      </DragDropContext>
+    </ThemeProvider>
   );
 }
 
